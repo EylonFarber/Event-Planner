6968ab690e07277035bca47031d08cc0903c3085
Eylon <eylonfar@gmail.com>
2025-07-03 21:19:32 +0200

    first commit, basic layout and routing

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router";
+import EventDetails from "./routes/EventDetails";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
+import Home from "./routes/Home";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import CreateEvent from "./SignUp.jsx/CreateEvent";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="eventdetails" element={<EventDetails />} />
+        <Route path="createevent" element={<CreateEvent />} />
+      </Routes>
+      <Link to={"/eventdetails"}>link</Link>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+
+<div className="navbar bg-primary text-primary-content">
+  <button className="btn m-auto btn-ghost text-xl">EVENT PLANNER</button>
+</div>  )
+}
