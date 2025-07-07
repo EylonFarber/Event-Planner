4e634e21a8563188e7ecbc09bb093a1f12b34436
Eylon <eylonfar@gmail.com>
2025-07-07 21:34:09 +0200

    fimal fiidter deisgn

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Button({text}) {
+  return (
+<button className="btn btn-primary">{text}</button>  )
+}
--- src/components/FooterIcon.jsx
+++ src/components/FooterIcon.jsx
@@ -1,1 +1,15 @@
-rfc
+import React, { Children } from "react";
+import { Routes, Route, Link } from "react-router";
+import { FaAmilia } from "react-icons/fa6";
+
+export default function FooterIcon({ linkTo, itemKey, content, icon }) {
+  return (
+    <Link to={linkTo} key={itemKey}>
+      {" "}
+      <li>
+        <p>{icon}</p>
+        <p className="text-[10px] self-center">{content}</p>
+      </li>
+    </Link>
+  );
+}
