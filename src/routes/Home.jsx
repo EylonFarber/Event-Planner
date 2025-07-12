3566ea48b15570a299ede144492850e938c26cad
Eylon <eylonfar@gmail.com>
2025-07-12 12:17:39 +0200

    create event for and context done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
+import { useAppContext } from "../context/appContext";
 
 export default function Home() {
+    const { allEvents, getAllEvents } = useAppContext();
+
   const [TagsArray, setTagsArray] = useState([2, 2, 2, 2, 22, 2, 2, 2, 2]);
-  const [eventsArray, setEventsArray] = useState([1, 1, 1, 1, 1, 1, 1]);
+
+  useEffect(
+    ()=>{getAllEvents()},[]
+
+  )
   return (
     <div className="h-fit bg-gray-200 w-screen pb-28 px-4">
       <div className="fixed top-16 right-0 px-2 py-2 w-screen z-40 bg-gray-200">
@@ -31,16 +38,15 @@ export default function Home() {
         </label>
       </div>
       <div className="flex flex-col pt-52 gap-2 bg-gray-200 h-full">
-        {eventsArray.map((i) => (
+ {allEvents?.map((i,x) => (
           <EventCard
-            title={"Event title"}
+            title={allEvents[x].title}
             img={
               "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
             }
-            descrtion={"event dexcription"}
+            descrtion={allEvents[x].description}
           />
-        ))}
-        <div className="badge badge-neutral badge-outline mt-2">
+        ))}  <div className="badge badge-neutral badge-outline mt-2">
           Load More Events
         </div>
       </div>
